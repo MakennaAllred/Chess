84dd1765cd6d070cec8a0b6d9653965b75ada5e5
MakennaAllred <makenna.foulkrod@gmail.com>
2024-01-22 18:21:42 -0700

    Implemented kingmoves and not null functions. Ran King tests and all 3 passed on the first run and I am flabbergasted

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Objects;
 
@@ -12,7 +13,7 @@ import java.util.Objects;
 public class ChessPiece {
     private ChessGame.TeamColor pieceColor;
     private PieceType type;
-    public ChessPiece(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type) {
+    public ChessPiece(ChessGame.TeamColor pieceColor, PieceType type) {
         this.pieceColor = pieceColor;
         this.type = type;
     }
@@ -28,6 +29,14 @@ public class ChessPiece {
     @Override
     public int hashCode() {
         return Objects.hash(pieceColor, type);
+    }
+
+    @Override
+    public String toString() {
+        return "ChessPiece{" +
+                "pieceColor=" + pieceColor +
+                ", type=" + type +
+                '}';
     }
 
     /**
@@ -65,58 +74,85 @@ public class ChessPiece {
      */
 
     public Collection<ChessMove> notNull(ChessBoard board, ChessPosition myPosition, ChessGame.TeamColor origColor, ChessPosition posPosition){
+        Collection<ChessMove> moves = new ArrayList<>();
         ChessPiece pos = board.getPiece(posPosition);
         if(pos == null){
-            new ChessMove(myPosition, posPosition, null);
+             moves.add(new ChessMove(myPosition, posPosition, null));
         }
         else {
             ChessGame.TeamColor posColor = pos.getTeamColor();
             if (origColor != posColor) {
-                new ChessMove(myPosition, posPosition, null);
-                // append to collection of possible moves
+               moves.add(new ChessMove(myPosition, posPosition, null));
             }
         }
+        return moves;
     }
 
     public Collection<ChessMove> kingMoves(ChessBoard board, ChessPosition myPosition, ChessPiece current) {
+        Collection<ChessMove> total = new ArrayList<>();
         int row = myPosition.getRow();
         int col = myPosition.getColumn();
         ChessGame.TeamColor origColor = current.getTeamColor();
         // up, check null first
         if (row + 1 < 8) {
             Collection<ChessMove> up = notNull(board, myPosition, origColor, new ChessPosition(row + 1, col));
+            total.addAll(up);
         }
         // down
         if (row - 1 > 0 && row - 1 < 8) {
             Collection<ChessMove> down = notNull(board, myPosition, origColor, new ChessPosition(row - 1, col));
+            total.addAll(down);
         }
         // right diagonal
         if (row + 1 < 8) {
             if (col + 1 < 8) {
                 Collection<ChessMove> urDiag = notNull(board, myPosition, origColor, new ChessPosition(row + 1, col +1));
+                total.addAll(urDiag);
             }
         }
         //upper left diagonal
         if (row + 1 < 8){
             if (col - 1 > 0 && col - 1 < 8){
                 Collection<ChessMove> ulDiag = notNull(board, myPosition, origColor, new ChessPosition(row + 1, col - 1));
+                total.addAll(ulDiag);
             }
         }
         // left
         if (col -1 > 0 && col +1 < 8){
             Collection<ChessMove> left = notNull(board, myPosition, origColor, new ChessPosition(row, col - 1));
+            total.addAll(left);
         }
         //right
         if(col + 1 < 8 ){
             Collection<ChessMove> right = notNull(board, myPosition, origColor, new ChessPosition(row, col + 1));
+            total.addAll(right);
+        }
+        //bt left
+        if (row - 1 > 0 && row - 1 < 8){
+            if(col - 1 > 0 && col - 1 < 8){
+                Collection<ChessMove> btleft = notNull(board, myPosition, origColor, new ChessPosition(row - 1, col - 1));
+                total.addAll(btleft);
+            }
+        }
+        //bt right
+        if (row - 1 > 0 && row -1 < 8){
+            if(col + 1 > 0 && col + 1 < 8){
+                Collection<ChessMove> btright = notNull(board, myPosition, origColor, new ChessPosition(row - 1, col + 1));
+                total.addAll(btright);
+            }
         }
 
-        return null;
+        return total;
     }
+
 //if it's going in the return collection it needs to be 1-based not 0-based
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition){
         ChessPiece cur = board.getPiece(myPosition);
-        return null;
+        if (cur.type == PieceType.KING){
+            return kingMoves(board,myPosition,cur);
+        }
+
+       return null;
     }
 }
 
